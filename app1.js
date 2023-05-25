d4c49f2fd2fae9863e5f3817d3af8da6bf9071ca
riya40 <priyanunna20@gmail.com>
2023-05-25 14:25:57 +0530

    Added:inserting the data to database

---
+++ app1.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql2');
+
+
+//create mysql connection
+
+const connnection = mysql.createConnection({
+    host:'127.0.0.1',
+    user:'root',
+    password:'Mysql#12345',
+    database:'company'
+});
+
+connnection.connect((err)=>{
+    if(err){
+        console.log('Error connecting to mysql database',err);
+        return;
+    }
+    console.log('connected to mysql')
+});
+
+// data to be inserted
+
+const data = {id:105,name:'molly',department:'it'}
+
+connnection.query('insert into employee set ?',data,(err,result)=>{
+    if(err){
+        console.error('error insertind data',err);
+        connnection.end()
+        return;
+    }
+    console.log('data inserted successfully');
+    console.log('inserted ID:',result.insertId);
+
+    connnection.end();
+});
+
+connnection.query('select * from employee', (err,result)=> {
+    //process the result of the query 
+
+    console.log('table data : ');
+    result.forEach((row) => {
+        console.log(row);
+    });
+
+    connnection.end();
+})
