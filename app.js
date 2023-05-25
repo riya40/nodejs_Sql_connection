9f8bd6374a7570c965f8de2cdcd8a4683ab3376b
riya40 <priyanunna20@gmail.com>
2023-05-25 11:30:55 +0530

    Added:Retieving data from table

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 
+
 //create mysql connection
 
 const connnection = mysql.createConnection({
@@ -15,4 +16,22 @@ connnection.connect((err)=>{
         return;
     }
     console.log('connected to mysql')
+});
+
+const query = 'select * from employee';
+connnection.query(query,(err,results)=>{
+    if(err){
+        console.error("error in executng query",err);
+        connnection.end();
+        return;
+    }
+
+    console.log('table data:');
+    results.forEach((row)=>{
+        console.log(row);
+    });
+
+    //close connection
+
+    connnection.end();
 });
